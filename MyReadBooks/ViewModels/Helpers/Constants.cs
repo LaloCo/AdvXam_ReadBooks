84267573bd509f67704dd9910d7eb64e548105d1
Eduardo Rosas <lalorosas@outlook.com>
2021-03-25 13:04:02 -0600

    Use and deserialize the Google Books API
    Now making the GET request to the API using HTTP and deserializing
    the JSON response to C# using the new classes found on GoogleBooksAPI.cs

---
+++ MyReadBooks/ViewModels/Helpers/Constants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace MyReadBooks.ViewModels.Helpers
+{
+    public class Constants
+    {
+        public const string GOOGLE_BOOKS_API_KEY = "AIzaSyAXDKHgBKhfl9USaNRilx-2LlKR1T8THSY";
+    }
+}
--- MyReadBooks/ViewModels/NewBookVM.cs
+++ MyReadBooks/ViewModels/NewBookVM.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Windows.Input;
+using MyReadBooks.Models;
+using MyReadBooks.ViewModels.Helpers;
+using Newtonsoft.Json;
+using Prism.Commands;
+
 namespace MyReadBooks.ViewModels
 {
     public class NewBookVM
     {
+        public ICommand SearchCommand { get; set; }
+
         public NewBookVM()
         {
+            SearchCommand = new DelegateCommand<string>(GetSearchResults);
+        }
+
+        private async void GetSearchResults(string query)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                var result = await client.GetStringAsync($"https://www.googleapis.com/books/v1/volumes?q={query}&key{Constants.GOOGLE_BOOKS_API_KEY}");
+
+                var data = JsonConvert.DeserializeObject<BoooksAPI>(result);
+            }
         }
     }
 }
